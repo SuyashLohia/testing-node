9a333f6140f0fd7fecf37d0fe98e64821c5e125d
Suyash Lohia <suyashlohia@Suyashs-MacBook-Pro.local>
2018-07-06 14:50:40 +0530

    File sent through express

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-console.log("Helloi World");
+var express= require('express');
+var app=express();
+	app.get( '/', function( req, res){
+		res.sendFile(__dirname +'/index.html');
+	});
+
+	app.listen(8081,function(){
+	console.log("Running at localhost:8081");
+	});
 
